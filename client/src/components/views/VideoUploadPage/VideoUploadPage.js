e90e670ac4b5d3977acf23b5a7e1d081e341c35a
noopy <young_ahn@yonsei.ac.kr>
2020-05-01 21:17:46 +0900

    make upload page

---
+++ client/src/components/views/VideoUploadPage/VideoUploadPage.js
@@ -0,0 +1,10 @@
+// making video upload page
+// functional component
+// download ES7 extension and type in rfce to shorten the process
+import React from "react";
+
+function VideoUploadPage() {
+  return <div></div>;
+}
+
+export default VideoUploadPage;
